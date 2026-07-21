77cb6b903eb03d3d91055132f2f43ea5cc87f643
Jayan Patel <jayanp0202@gmail.com>
2026-07-20 19:58:05 -0500

    reduce scope

--- fsr-test.ino
+++ fsr-test.ino
@@ -1,30 +1,26 @@
 /*
   fsr tester
   A4 = index
-  A5 = middle
 */
 
-const uint8_t FSR_PIN[2] = {A4, A5};
+const uint8_t FSR_PIN[1] = {A4};
 const unsigned long PRINT_INTERVAL = 100;
 
 unsigned long lastPrint = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("FSR Tester");
 }
 
 void loop() {
 
   int fsr1 = analogRead(FSR_PIN[0]);
-  int fsr2 = analogRead(FSR_PIN[1]);
 
   if (millis() - lastPrint >= PRINT_INTERVAL) {
     lastPrint = millis();
 
     Serial.print("Index: ");
-    Serial.print(fsr1);
-    Serial.print("\tMiddle: ");
-    Serial.println(fsr2);
+    Serial.println(fsr1);
   }
 }
